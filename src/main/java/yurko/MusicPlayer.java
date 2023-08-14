48bb529f3485da48b75649bb3e63784a272535af
lucifer116664 <lucifer116664@gmail.com>
2023-08-14 20:44:38 +0300

    Qualifier annotation

--- src/main/java/yurko/MusicPlayer.java
+++ src/main/java/yurko/MusicPlayer.java
@@ -1,19 +1,23 @@
 package yurko;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    /*@Autowired
+    @Qualifier("rockMusic")*/
     private Music music;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic music) {
+    public MusicPlayer(@Qualifier("classicalMusic") Music music) {
         this.music = music;
     }
 
     /*@Autowired
-    public void setMusic(RockMusic music) {
+    @Qualifier("rockMusic")
+    public void setMusic(Music music) {
         this.music = music;
     }*/
 
